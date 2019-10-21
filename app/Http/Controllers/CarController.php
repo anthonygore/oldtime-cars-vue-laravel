ab4331e757fe80293350a32add25ecced3a081c1
Dave Mingos <davemingos@me.com>
2019-10-21 08:29:06 -0400

    Refactor the get_cars method

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -6,16 +6,16 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
-    public function get_cars($id) {
+    private function get_cars($id = null) {
         $cars = [
             [ 'id' => 1, 'name' => 'Buick', 'url' => asset('images/buick.png'), 'price' => '$15,000' ],
             [ 'id' => 2, 'name' => 'Chevy', 'url' => asset('images/chevy.png'), 'price' => '$18,000' ],
         ];
-        if (!$id) {
-            return [ 'cars' => $cars ];
-        } else {
-            return $cars[$id - 1];
+        if (is_null($id)) {
+            return compact('cars');
         }
+        $key = $id - 1;
+        return array_key_exists($key, $cars) ? $cars[$key] : null;
     }
 
     public function detail_web($id)
@@ -32,13 +32,13 @@ class CarController extends Controller
 
     public function home_web()
     {
-        $state = array_merge([ 'path' => '/' ], $this->get_cars(NULL));
+        $state = array_merge([ 'path' => '/' ], $this->get_cars());
         return view('app', ['state' => $state]);
     }
 
     public function home_api()
     {
-        $state = array_merge([ 'path' => '/' ], $this->get_cars(NULL));
+        $state = array_merge([ 'path' => '/' ], $this->get_cars());
         return response()->json($state);
     }
 }
